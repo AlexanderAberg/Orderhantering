ad6ef81f87413e5beacd9806c47212d6b9991b0a
Alexander Åberg <alexander_aberg1985@msn.com>
2025-09-10 18:01:17 +0200

    Created Order class

---
+++ ITSystem/Data/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSystem.Data.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Status { get; set; } = "Pending";
+    }
+}
