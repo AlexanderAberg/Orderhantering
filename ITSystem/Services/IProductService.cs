281a5c4ad3817bd6acb437ec06cdd2cd134fdb8a
Alexander Åberg <alexander_aberg1985@msn.com>
2025-09-11 19:23:48 +0200

    Created IProductService and ProductService with code

---
+++ ITSystem/Services/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using ITSystem.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSystem.Services
+{
+    public interface IProductService
+    {
+        Product? GetById(int id);
+        IEnumerable<Product> GetAll();
+        void Create(Product product);
+        void Update(Product product);
+        void Delete(int id);
+    }
+
+}
---
+++ ITSystem/Services/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using ITSystem.Data.Contexts;
+using ITSystem.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSystem.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly OrderDbContext _db;
+
+        public ProductService(OrderDbContext db)
+        {
+            _db = db;
+        }
+
+        public Product? GetById(int id)
+        {
+            return _db.Products.Find(id);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _db.Products.ToList();
+        }
+
+        public void Create(Product product)
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+            _db.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var product = _db.Products.Find(id);
+            if (product != null)
+            {
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+            }
+        }
+    }
+
+}
