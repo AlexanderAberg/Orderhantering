3a523b044801e3d392b5419461aedbba2bd16b7d
Alexander Åberg <alexander_aberg1985@msn.com>
2025-09-13 17:39:25 +0200

    Created OTSystem project with boilerplate code for Program.cs

---
+++ OTSystem/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace OTSystem
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var app = scope.ServiceProvider.GetRequiredService<OTApp>();
+            await app.RunAsync();
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddHttpClient();
+                    services.AddScoped<ITIntegrationService>();
+                    services.AddScoped<ProductionLineService>();
+                    services.AddScoped<OTApp>();
+                });
+    }
+}
